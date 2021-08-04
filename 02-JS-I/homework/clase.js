6a8e0f9f9b67b648cccdf655e9e106fbab221d97
Maern Oud <maernoud@gmail.com>
2021-08-04 13:17:46 -0300

    modified:   homework.js

---
+++ 02-JS-I/homework/clase.js
@@ -0,0 +1,16 @@
+var nombre = 'manuel'
+var edad = 35
+
+function prueba (nombre, edad) {
+    console.log("hola como estas?")
+    console.log("mi nombre es " + nombre)
+    console.log("mi edad es "+ edad + " años.")
+    return('fuckyou')
+}
+
+function prueba2 (prueba ) {
+    console.log(prueba)
+    console.log('espero que estes bien.')
+    return('fuckyou too')
+}
+
